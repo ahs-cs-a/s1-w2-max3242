f26e61562691d11322f69cf51e6d6e376cc9c25e
max3242 <maximiliandc26@ausdk12.org>
2024-08-23 04:47:31 +0000

    a

--- Human.java
+++ Human.java
@@ -1,12 +1,27 @@
 public class Human {
     private String name;
     private String eyeColor;
-   
-    public Human(String n, String e){
-        name = n;
-        eyeColor = e;
+    private int age;
+    public Human(String n, String e) {
+        name=n; 
+        eyeColor=e;
+        printState();
     }
- 
+    public Human(String n, int a) {
+        name=n;
+        age=a;
+        printState();
+    }
+    public Human(int a, String e) {
+        age=a;
+        eyeColor=e;
+        printState();
+    }       public Human(String n, String e, int a) {
+        name=n;
+        eyeColor=e;
+        age=a;
+        printState();
+    }
     public void walking(){
         System.out.println("Walking ...");
     }
@@ -23,6 +38,7 @@ public class Human {
     public void printState(){
         System.out.println("Name: " + name);
         System.out.println("Eye Color: " + eyeColor);
+        System.out.println("Age: "+age);
     }
  }
  
